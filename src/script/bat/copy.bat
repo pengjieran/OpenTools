bef2fd00e5799417482659eed0dfcfb8ecb7e9d2
Jieran.Peng <jieran.peng@gmail.com>
2014-08-20 10:23:29 +0800

    提交与windows下bat操作相关的文件，包含数据备份还原import.bat和复制文件copy.bat脚本

---
+++ src/script/bat/copy.bat
@@ -0,0 +1,2 @@
+@echo off
+copy D:\workspace\soomey.framework\trunk\libs\soomey.jar D:\workspace\soomey\trunk\libs
---
+++ src/script/bat/import.bat
@@ -0,0 +1,3 @@
+@echo off
+@echo "waiting to import data......"
+mysql -uroot -proot soomey < soomey_net.dmp

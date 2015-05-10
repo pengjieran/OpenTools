f154482df2201160c4aa56c3010bd1c6534487fd
Aaron <pjr0206@163.com>
2015-05-10 11:11:23 +0800

    文件格式修正，

--- src/java/main/com/opentools/file/bigfile/Main.java
+++ src/java/main/com/opentools/file/bigfile/Main.java
@@ -4,16 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		BigFileReader.Builder builder = new BigFileReader.Builder(
-				"e:/reliability.txt", new FileHandle() {
+		BigFileReader.Builder builder = new BigFileReader.Builder("e:/reliability.txt", new FileHandle() {
 
 					@Override
 					public void handle(String line) {
-						// System.out.println(line);
-						// increat();
+						System.out.println(line);
+						//increat();
 					}
 				});
-		builder.withTreahdSize(10).withCharset("gbk")
+		builder.withTreahdSize(10).withCharset("utf8")
 				.withBufferSize(1024 * 1024);
 		BigFileReader bigFileReader = builder.build();
 		bigFileReader.start();

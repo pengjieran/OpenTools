c0dbac0c9c3584d66f02818cbbf57c71d648f756
Aaron <jieran.peng@mybean.cn>
2015-05-29 14:50:00 +0800

    rsa加密，目前只能加密int数字

---
+++ src/java/main/com/opentools/common/RSA.java
@@ -0,0 +1,64 @@
+package com.opentools.common;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+public class RSA
+{
+	private final static BigInteger one = new BigInteger("1");
+
+	private BigInteger modulus;
+
+	private BigInteger publicKey;
+	private BigInteger privateKey;
+
+	public RSA(int bitLength)
+	{
+		BigInteger p = BigInteger.probablePrime(bitLength / 2, new Random());
+		BigInteger q = BigInteger.probablePrime(bitLength / 2, new Random());
+		BigInteger phi = (p.subtract(one)).multiply(q.subtract(one));
+
+		this.modulus = p.multiply(q);
+		this.publicKey = new BigInteger("65537");
+		this.privateKey = publicKey.modInverse(phi);
+
+	}
+
+	public BigInteger encrypt(BigInteger message)
+	{
+		return message.modPow(publicKey, modulus);
+	}
+
+	public BigInteger decrypt(BigInteger encrypted)
+	{
+		return encrypted.modPow(privateKey, modulus);
+	}
+
+	public String encrypt(String message)
+	{
+		return this.encrypt(new BigInteger(message)).toString();
+	}
+
+	public String decrypt(String message)
+	{
+		return this.decrypt(new BigInteger(message)).toString();
+	}
+
+	public BigInteger getModulus()
+	{
+		return this.modulus;
+	}
+
+	public String getPublicKey()
+	{
+		return this.publicKey.toString() + this.modulus.toString();
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Public:\t" + this.publicKey + "Private:\t" + this.privateKey
+				+ "Modulus:\t" + this.modulus;
+	}
+
+}
---
+++ src/java/test/com/test/RSATest.java
@@ -0,0 +1,19 @@
+package test;
+
+import com.opentools.common.RSA;
+
+public class RSATest
+{
+
+	public static void main(String[] args)
+	{
+		
+		RSA rsa = new RSA(5);
+		String encrypt = rsa.encrypt("15");
+		System.out.println(encrypt);
+		String decrypt = rsa.decrypt(encrypt);
+		System.out.println(decrypt);
+		
+	}
+
+}

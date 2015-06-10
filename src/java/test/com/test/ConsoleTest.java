4285b2ab81b08832991f4413bc2fe607e27b1e6e
Aaron <pjr0206@163.com>
2015-06-10 22:24:41 +0800

    增加部分测试代码

--- src/java/test/com/test/ConsoleTest.java
+++ src/java/test/com/test/ConsoleTest.java
@@ -1,16 +1,12 @@
 package test;
 
-import java.io.IOException;
-
-import com.google.zxing.NotFoundException;
-import com.google.zxing.Result;
-import com.google.zxing.WriterException;
-import com.opentools.web.QRUtils;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ConsoleTest {
 	
 	public static void main(String[] args) {
-		try {
+		/*try {
 			QRUtils.encode("我的疯狂只为你而存在", "aa.png", 300, QRUtils.ENCODER_MODE_UTF8);
 			
 		} catch (WriterException e) {
@@ -31,7 +27,10 @@ public class ConsoleTest {
 		} catch (IOException e) {
 			
 			e.printStackTrace();
-		}
+		}*/
+		Date date = new Date(1433677228923L);
+		SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
+		System.out.println(format.format(date));
 	}
 
 }

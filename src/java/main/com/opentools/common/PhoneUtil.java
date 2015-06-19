849d51972e7f7f6bcd5ccbb0a663cc5c0d773095
Aaron <jieran.peng@mybean.cn>
2015-06-19 19:07:12 +0800

    增加解析手机号和电话号码的工具类

---
+++ src/java/main/com/opentools/common/PhoneUtil.java
@@ -0,0 +1,48 @@
+package com.opentools.common;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.PhoneNumberUtil.PhoneNumberFormat;
+import com.google.i18n.phonenumbers.Phonenumber.PhoneNumber;
+
+/**
+ * 
+ * @author Aaron
+ *识别手机号，电话号码
+ */
+public class PhoneUtil {
+	
+	public static final String IS_VALID_NUMBER = "isValidNumber";
+	
+	public static final String COUNTRY_CODE = "countryCode";
+	
+	public static final String NATIONAL_NUMBER = "national_number";
+	
+	public static final String E164_NUMBER = "E164_number";
+
+	private static PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.getInstance();
+	
+	public static Map<String, String> getPhoneNumber(String phone, String defaultRegion) throws NumberParseException
+	{
+		Map<String, String> phones = new LinkedHashMap<String, String>();
+		
+		if (null == defaultRegion)
+		{
+			defaultRegion = "CN";
+		}
+		
+		PhoneNumber phoneNumber = phoneNumberUtil.parse(phone, defaultRegion);
+		boolean isvalidNumber = phoneNumberUtil.isValidNumber(phoneNumber);		
+		
+		phones.put(IS_VALID_NUMBER, String.valueOf(isvalidNumber));
+		phones.put(COUNTRY_CODE, String.valueOf(phoneNumber.getCountryCode()));
+		phones.put(NATIONAL_NUMBER, String.valueOf(phoneNumber.getNationalNumber()));
+		phones.put(E164_NUMBER, isvalidNumber ? phoneNumberUtil.format(phoneNumber, PhoneNumberFormat.E164) : "invalid");
+		
+		return phones;
+	}
+	private PhoneUtil(){}
+}

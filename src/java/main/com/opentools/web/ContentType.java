32aa05654a0bce3d9d8aa3affb607a8d28b37c4d
Aaron <jieran.peng@mybean.cn>
2016-02-27 20:30:44 +0800

    增加视频和网页的contenttype

--- src/java/main/com/opentools/web/ContentType.java
+++ src/java/main/com/opentools/web/ContentType.java
@@ -7,6 +7,14 @@ package com.opentools.web;
  */
 public class ContentType {
 	
+	public static final String CONTENTTYPE_KEY = "Content-type";
+	
+	public static final String CONTENTTYPE_OCTET_VALUE = "application/octet-stream";//文件扩展名.*,二进制流，不知道下载文件类型
+	
+	public static final String CONTENTTYPE_VIDEO_AVI_VALUE = "video/avi";//文件扩展名.avi,视频文件
+	
+	public static final String CONTENTTYPE_HTML_VALUE = "text/html";//网页文件，包含html或者jsp等文件
+	
 	private ContentType() {}
 
 }

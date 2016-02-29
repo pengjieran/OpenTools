e34a298d181f1b381d8b5df7633f35cd3efbce39
Aaron <pengjieran@gmail.com>
2016-02-29 16:33:48 +0800

    增加向配置文件中写入数据的方法

--- src/java/main/com/opentools/common/PropertiesUtil.java
+++ src/java/main/com/opentools/common/PropertiesUtil.java
@@ -3,7 +3,9 @@ package com.opentools.common;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Properties;
 
 /**
@@ -13,6 +15,11 @@ import java.util.Properties;
  */
 public class PropertiesUtil {
 
+	/**
+	 * 读取配置文件
+	 * @param srcFile 文件绝对路径
+	 * @return
+	 */
 	public static Properties loadProperties(String srcFile) {
 		
 		Properties properties = new Properties();
@@ -29,6 +36,26 @@ public class PropertiesUtil {
 		}
 		
 		return null;
+	}
+	
+	/**
+	 * 向配置文件中写入数据
+	 * @param srcFile 文件绝对路径
+	 * @param key
+	 * @param value
+	 */
+	public static void writeData(String srcFile, String key, String value) {
+		
+		Properties prop = loadProperties(srcFile);
+		try {
+
+			OutputStream fos = new FileOutputStream(srcFile);
+			prop.setProperty(key, value);
+			prop.store(fos, "Update '" + key + "' value");
+			fos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private PropertiesUtil() {}

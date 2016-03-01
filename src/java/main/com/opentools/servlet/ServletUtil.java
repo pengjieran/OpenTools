fcc8c8f799b51f0881f8d7c890696b96d9aa3afb
Aaron <pengjieran@gmail.com>
2016-03-01 13:42:20 +0800

    自动部署工具在部署完成后返回json格式的部署结果

--- src/java/main/com/opentools/servlet/ServletUtil.java
+++ src/java/main/com/opentools/servlet/ServletUtil.java
@@ -4,13 +4,19 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.util.Collection;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.http.entity.ContentType;
+
+import com.opentools.charset.CharsetUtil;
 
 /**
  * 处理http请求相关的工具类
@@ -24,7 +30,7 @@ public class ServletUtil {
 	 * @param request
 	 * @param path 相对当前项目的文件路径
 	 */
-	public static void UploadFile(final HttpServletRequest request, String path) {
+	public static void UploadFile(final HttpServletRequest request, String path, final HttpServletResponse response) {
 		
 		String realPath = request.getServletContext().getRealPath("");
 		
@@ -49,6 +55,13 @@ public class ServletUtil {
 					fos.close();
 				}
 			}
+			
+			response.setContentType(ContentType.create(ContentType.APPLICATION_JSON.getMimeType(), Charset.forName(CharsetUtil.UTF_8)).getMimeType());
+			String s = new String("{\"key\":\"value\"}");
+			ServletOutputStream outputStream = response.getOutputStream();
+			IOUtils.write(s.getBytes("UTF-8"), outputStream);
+			outputStream.flush();
+			outputStream.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ServletException e) {

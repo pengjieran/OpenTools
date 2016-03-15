75f2fdbacc57db902fc941877bca16f794bdb780
Aaron <pengjieran@gmail.com>
2016-03-15 10:13:39 +0800

    增加输出工具类，用于输出map,list或者其它什么类型的数据，主要用于调试输出数据，又不想使用json转换数据

---
+++ src/java/main/com/opentools/common/PrintUtil.java
@@ -0,0 +1,43 @@
+package com.opentools.common;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 输出相关的工具类，主要用于各种格式的数据输出
+ * @author aaron
+ *
+ */
+public class PrintUtil {
+	
+	/**
+	 * 将一个map转换为字符串输出，输出格式为{"key"：value}
+	 * @param map
+	 * @return
+	 */
+	public static StringBuilder printMap(Map<String, Object> map) {
+		
+		StringBuilder strBuilder = new StringBuilder();
+		Set<String> keySet = map.keySet();
+		List<String> keys = new ArrayList<>(keySet);
+		
+		strBuilder.append("{");
+		for (String key : keys) {
+			
+			strBuilder.append("\"" + key + "\":");
+			strBuilder.append(map.get(key).toString());
+			
+			if (!key.equals(keys.get(keys.size() - 1))) {
+				
+				strBuilder.append(",");
+			}
+		}
+		
+		strBuilder.append("}");
+		
+		return strBuilder;
+	}
+
+}

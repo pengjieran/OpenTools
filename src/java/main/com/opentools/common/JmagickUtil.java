304af2836ce94f5cc76306ca40d1f10ee8169c97
DeanAaron <pengjieran@gmail.com>
2016-03-20 20:59:30 +0800

    增加jmagick工具类

--- src/java/main/com/opentools/common/JmagickUtil.java
+++ src/java/main/com/opentools/common/JmagickUtil.java
@@ -1,7 +1,15 @@
 package com.opentools.common;
 
 /**
+ * 操作jmajick的工具类，主要用于图像处理
  * Created by Aaron on 2016/3/20.
  */
 public class JmagickUtil {
+
+    /**
+     * 剪裁图片，设置为指定大小
+     */
+    public static void resetSize() {
+
+    }
 }

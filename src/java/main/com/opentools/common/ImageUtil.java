528fdf4b9c2cffc726cad6143bbdfcae4422c760
Aaron <pengjieran@gmail.com>
2016-03-21 10:04:15 +0800

    更正类名

--- src/java/main/com/opentools/common/ImageUtil.java
+++ src/java/main/com/opentools/common/ImageUtil.java
@@ -5,7 +5,7 @@ package com.opentools.common;
  * 更新于2016年3月21日	放弃使用jmagick，在长时间运行后，会出现JMagick使用JNI技术调用ImageMagick，存在JNI-hazard问题，尝试改用im4java
  * Created by Aaron on 2016/3/20.
  */
-public class JmagickUtil {
+public class ImageUtil {
 
     /**
      * 剪裁图片，设置为指定大小

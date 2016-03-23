8a99de78bcd6f6859ed54f132d98833246e7939b
Aaron <pengjieran@gmail.com>
2016-03-23 10:29:23 +0800

    增加一个将inputstream转换为string的工具方法

--- src/java/main/com/jdk/StreamUtil.java
+++ src/java/main/com/jdk/StreamUtil.java
@@ -1,5 +1,9 @@
 package com.jdk;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * 流处理工具类
  * @author aaron
@@ -7,5 +11,23 @@ package com.jdk;
  * @version 0.0.1
  */
 public class StreamUtil {
+	
+	final static int BUFFER_SIZE = 4096;
+	
+	public static String InputStreamTOString(InputStream inputStream) throws IOException {
+		
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		byte[] data = new byte[BUFFER_SIZE];
+		String string = null;
+		int count = 0;
+		while ((count = inputStream.read(data, 0, BUFFER_SIZE)) != -1) {
+			
+			byteArrayOutputStream.write(data, 0, count);
+			
+		}
+		
+		string = new String(byteArrayOutputStream.toByteArray(), "UTF-8");
+		return string;
+	}
 
 }

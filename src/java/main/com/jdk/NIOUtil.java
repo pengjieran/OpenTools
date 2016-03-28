153b04bf01d4cc924b41544070a8d22959135157
Aaron <pengjieran@gmail.com>
2016-03-28 11:00:37 +0800

    增加将文件读入到管道中的工具方法

--- src/java/main/com/jdk/NIOUtil.java
+++ src/java/main/com/jdk/NIOUtil.java
@@ -1,5 +1,9 @@
 package com.jdk;
 
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.channels.Channel;
+
 /**
  * 处理与nio相关的工具类
  * @author Aaron
@@ -7,4 +11,12 @@ package com.jdk;
  */
 public class NIOUtil {
 
+	@SuppressWarnings("resource")
+	public static Channel fileToChannel(String filePath) throws FileNotFoundException {
+		
+		RandomAccessFile randomAccessFile = new RandomAccessFile(filePath, "rw");
+		
+		return randomAccessFile.getChannel();
+		
+	}
 }

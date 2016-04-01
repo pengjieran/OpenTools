a0d96416cf73a36e7e5ed99c21ba8aa616cb7d5f
Aaron <pengjieran@gmail.com>
2016-04-01 10:50:29 +0800

    增加将channel中的数据写入文件的工具方法

--- src/java/main/com/jdk/NIOUtil.java
+++ src/java/main/com/jdk/NIOUtil.java
@@ -1,8 +1,16 @@
 package com.jdk;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
 import java.nio.channels.Channel;
+import java.nio.channels.FileChannel;
+
+import org.apache.commons.io.IOUtils;
 
 /**
  * 处理与nio相关的工具类
@@ -17,6 +25,31 @@ public class NIOUtil {
 		RandomAccessFile randomAccessFile = new RandomAccessFile(filePath, "rw");
 		
 		return randomAccessFile.getChannel();
-		
 	}
+	
+	public static void readFromChannel(Channel channel, String filePath) throws FileNotFoundException {
+		
+		OutputStream out = new FileOutputStream(new File(filePath));
+		FileChannel fileChannel = (FileChannel) channel;
+		ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+		try {
+			int read = fileChannel.read(byteBuffer);
+			while (read != -1) {
+				
+				byteBuffer.flip();
+				while (byteBuffer.hasRemaining()) {
+					
+					byte b = byteBuffer.get();
+					byte[] bytes = new byte[]{b};
+					IOUtils.write(bytes, out);
+				}
+				
+				byteBuffer.clear();
+				read = fileChannel.read(byteBuffer);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }

573cdbe31a34e2da28a0a972c511d0c6174595f0
Aaron <pengjieran@gmail.com>
2016-04-05 09:19:40 +0800

    新增利用ThreadLocal存储数据的工具类，线程不安全，请仔细考虑

---
+++ src/java/main/com/jdk/ThreadLocalContext.java
@@ -0,0 +1,38 @@
+package com.jdk;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 将数据存储于本地threadlocal中
+ * @author aaron
+ *
+ */
+public class ThreadLocalContext {
+	
+	private static ThreadLocal<Map<String, Object>> threadLocal = new ThreadLocal<Map<String, Object>>(){
+		
+		@Override
+		protected Map<String, Object> initialValue() {
+			
+			return new HashMap<String, Object>();
+		}
+		
+	};
+
+	public static void put(String key, Object value) {
+		
+		threadLocal.get().put(key, value);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T get(String key) {
+		
+		return ((T) threadLocal.get().get(key));
+	}
+	
+	public static void reset() {
+		
+		threadLocal.get().clear();
+	}
+}

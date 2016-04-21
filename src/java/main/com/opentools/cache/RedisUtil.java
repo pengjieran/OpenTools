51abe2d850cd5e1fd38bc9e8e9114487fbc57227
Aaron <pengjieran@gmail.com>
2016-04-21 18:53:55 +0800

    增加redis相关操作

--- src/java/main/com/opentools/cache/RedisUtil.java
+++ src/java/main/com/opentools/cache/RedisUtil.java
@@ -5,7 +5,9 @@ import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.exceptions.JedisConnectionException;
 
 import java.net.SocketTimeoutException;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Aaron on 2016/4/17.
@@ -84,5 +86,60 @@ public class RedisUtil {
 
         returnResource(jedis);
     }
-    
+
+    /**
+     * 向redis缓存中新增map
+     * @param key
+     * @param map
+     */
+    public static void setMap(String key, Map<String, String> map) {
+
+        Jedis jedis = getResource();
+        assert jedis != null;
+        jedis.hmset(key, map);
+
+        returnResource(jedis);
+    }
+
+    /**
+     * 获取map中key的个数
+     * @param key
+     * @return
+     */
+    public static long getMapLength(String key) {
+
+        Jedis jedis = getResource();
+        Long length = jedis.hlen(key);
+        returnResource(jedis);
+
+        return length;
+    }
+
+    /**
+     * 获取map中key值列表
+     * @param key
+     * @return
+     */
+    public static Set<String> getKeys(String key) {
+
+        Jedis jedis = getResource();
+        final Set<String> keys = jedis.hkeys(key);
+        returnResource(jedis);
+
+        return keys;
+    }
+
+    /**
+     * 获取map中的value列表
+     * @param key
+     * @return
+     */
+    public static List<String> getValues(String key) {
+
+        Jedis jedis = getResource();
+        final List<String> values = jedis.hvals(key);
+        returnResource(jedis);
+
+        return values;
+    }
 }

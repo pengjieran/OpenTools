adc011597dc1d13f82e27b7b15c2d0a87702d355
Aaron <pengjieran@gmail.com>
2016-06-06 18:48:50 +0800

    修正类为接口，不允许实例化，只提供静态常量

--- src/java/main/com/opentools/web/ContentType.java
+++ src/java/main/com/opentools/web/ContentType.java
@@ -2,23 +2,21 @@ package com.opentools.web;
 
 /**
  * 表明发送请求时contentType部分的key和value值
- * @author Aaron
  *
+ * @author Aaron
  */
-public class ContentType {
-	
-	public static final String CONTENTTYPE_KEY = "Content-type";
-	
-	public static final String CONTENTTYPE_OCTET_VALUE = "application/octet-stream";//文件扩展名.*,二进制流，不知道下载文件类型
-	
-	public static final String CONTENTTYPE_VIDEO_AVI_VALUE = "video/avi";//文件扩展名.avi,视频文件
-	
-	public static final String CONTENTTYPE_HTML_VALUE = "text/html";//网页文件，包含html或者jsp等文件
-	
-	public static final String CONTENTTYPE_JSON_VALUE = "application/json";//返回的是json数据
+public interface ContentType {
 
-	public static final String CONTENTTYPE_JSON_UTF8_VALUE = "application/json,charset=utf8";//utf8编码的json数据
+    String CONTENTTYPE_KEY = "Content-type";
 
-	private ContentType() {}
+    String CONTENTTYPE_OCTET_VALUE = "application/octet-stream";//文件扩展名.*,二进制流，不知道下载文件类型
+
+    String CONTENTTYPE_VIDEO_AVI_VALUE = "video/avi";//文件扩展名.avi,视频文件
+
+    String CONTENTTYPE_HTML_VALUE = "text/html";//网页文件，包含html或者jsp等文件
+
+    String CONTENTTYPE_JSON_VALUE = "application/json";//返回的是json数据
+
+    String CONTENTTYPE_JSON_UTF8_VALUE = "application/json,charset=utf8";//utf8编码的json数据
 
 }

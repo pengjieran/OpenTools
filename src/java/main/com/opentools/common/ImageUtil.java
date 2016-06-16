f5ef9c05463eeecc3944dad7a51848a05f7ffcac
Aaron <pengjieran@gmail.com>
2016-06-16 18:56:45 +0800

    update

--- src/java/main/com/opentools/common/ImageUtil.java
+++ src/java/main/com/opentools/common/ImageUtil.java
@@ -2,7 +2,7 @@ package com.opentools.common;
 
 /**
  * 操作jmajick的工具类，主要用于图像处理
- * 更新于2016年3月21日	放弃使用jmagick，在长时间运行后，会出现JMagick使用JNI技术调用ImageMagick，存在JNI-hazard问题，尝试改用im4java
+ * 更新于2016年3月21日	放弃使用jmagick，在长时间运行后，JMagick使用JNI技术调用ImageMagick，存在JNI-hazard问题，尝试改用im4java
  * Created by Aaron on 2016/3/20.
  */
 public class ImageUtil {

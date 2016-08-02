806def0ea611269482d37317fb4f24a1d03f6a82
Aaron <pengjieran@gmail.com>
2016-08-02 21:33:19 +0800

    update

--- src/java/main/com/opentools/web/HttpUtils.java
+++ src/java/main/com/opentools/web/HttpUtils.java
@@ -45,7 +45,7 @@ public class HttpUtils {
             /**打开和URL之间的连接**/
             URLConnection connection = realUrl.openConnection();
             /**设置通用的请求属性**/
-            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("accept", "application/json");
             connection.setRequestProperty("connection", "Keep-Alive");
             connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
             /**建立实际的连接**/

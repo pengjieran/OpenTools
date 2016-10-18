970a4c55e86ce71bf0fe45104a3b9b4231e9b11d
Aaron <pengjieran@gmail.com>
2016-10-18 21:26:06 +0800

    增加操作数据库的工具

--- src/java/main/com/opentools/db/DbUtils.java
+++ src/java/main/com/opentools/db/DbUtils.java
@@ -1,7 +1,22 @@
 package com.opentools.db;
 
+import java.sql.Connection;
+
 /**
- * Created by Aaron on 2016/10/18.
+ * 操作数据库相关的工具方法
  */
 public class DbUtils {
+
+    /**
+     * 获取数据库连接
+     * @param dbType
+     * @param dbName
+     * @param userName
+     * @param password
+     * @return
+     */
+    public static Connection getConnection(String dbType, String dbName, String userName, String password) {
+
+        return null;
+    }
 }
